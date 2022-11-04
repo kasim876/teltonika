8c8691cc01253dd02449b2233b4b463bb391f154
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-11-04 21:06:11 +0300

    feature: add parallax in hero section

--- src/partials/index/hero.js
+++ src/partials/index/hero.js
@@ -1,3 +1,14 @@
+document.querySelector(".hero").addEventListener("mousemove", (e) => {
+  const mask = document.getElementById('mask');
+  const maskParent = mask.closest('svg');
+  const offset = (document.body.clientWidth - maskParent.clientWidth) / 2;
+
+  let x = e.pageX - 250 - offset;
+  let y = e.pageY - 250;
+  
+  mask.setAttribute('transform', `translate(${x}, ${y})`)
+})
+
 document.querySelectorAll("[data-max]").forEach(el => {
   let currentValue = el.innerHTML;
   const maxValue = el.dataset.max;
